c3bbfed6402bd15e11bf7ac73f4906e0fc0dc5f0
Bruno <bripoll.19@gmail.com>
2021-02-25 21:37:54 -0300

    algunas modificaciones

--- fintrimestre/vaca.js
+++ fintrimestre/vaca.js
@@ -1,6 +1,7 @@
 let vaca = document.getElementById('vaca');
 let barra = document.getElementById('barra');
 let content = document.getElementById('content');
+let dia = document.getElementById('dia');
 
 let date = new Date();
 let deMiliADia = 1000*60*60*24;
@@ -12,7 +13,7 @@ let finalTrimestre = Math.floor((new Date(date.getFullYear(),mesFinal-1,diaFinal
 //cuando llegue a la fecha esto va a retornar un valor negativo, porque date es mayor que la fecha
 let finalTrimestre_100 = finalTrimestre/100;
 
-let diaComienzo = 9;
+let diaComienzo = 20;
 let mesComienzo = 2;
 //esto me genera la barra creciente
 let diaActual = Math.floor((date -  new Date(date.getFullYear(),mesComienzo-1,diaComienzo))/deMiliADia);
@@ -20,7 +21,17 @@ let porcentajeFaltante = diaActual/finalTrimestre*100;
 
 barra.style.width = `${porcentajeFaltante}%`;
 
-vaca.innerHTML = `RECORDATORIO: falta ${finalTrimestre} dias para que termine el 1º trimestre`;
+vaca.innerHTML = `RECORDATORIO: faltan <span>${finalTrimestre} dias</span> para que termine el 1º trimestre`;
+
+let emoticones = ['😎','😁','😀','😤','😊','😏','😑','😕','😟','😰','😨'];
+let emoDistancia = Math.floor(100/emoticones.length);
+for(i=0; i<emoticones.length; i++) {
+    if(([i]*emoDistancia)<=porcentajeFaltante) {
+        dia.innerHTML = `${finalTrimestre} dias ${emoticones[i]}`;
+    }
+}
+
+
 
 content.addEventListener('mouseover', function() {
     barra.style.width = `0%`;
