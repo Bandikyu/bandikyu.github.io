3d09e0aaa095075ca5b72838d2fa27ccdf0eedb2
Bandikyu <58412075+Bandikyu@users.noreply.github.com>
2021-09-12 17:00:58 -0300

    Update vaca.js

--- fintrimestre/vaca.js
+++ fintrimestre/vaca.js
@@ -6,15 +6,15 @@ let dia = document.getElementById('dia');
 let date = new Date();
 let deMiliADia = 1000*60*60*24;
 
-let diaFinal = 10;
-let mesFinal = 9;
+let diaFinal = 17;
+let mesFinal = 12;
 
 let finalTrimestre = Math.floor((new Date(date.getFullYear(),mesFinal-1,diaFinal+1) - date)/(deMiliADia));
 //cuando llegue a la fecha esto va a retornar un valor negativo, porque date es mayor que la fecha
 let finalTrimestre_100 = finalTrimestre/100;
 
-let diaComienzo = 30;
-let mesComienzo = 5;
+let diaComienzo = 13;
+let mesComienzo = 9;
 let diaActualTrimestre = Math.floor((date -  new Date(date.getFullYear(),mesComienzo-1,diaComienzo))/deMiliADia);
 let rangoDiasTrimestre = (((new Date(date.getFullYear(),mesFinal-1,diaFinal))-(new Date(date.getFullYear(),mesComienzo-1,diaComienzo)))/deMiliADia);
 let porcentajeRecorrido = 100/rangoDiasTrimestre*diaActualTrimestre;
