6883efff01a2bcb2d271c5e17b541ca6d80a4b71
Bru <bripoll.18@gmail.com>
2022-04-22 00:40:58 -0300

    second

--- script.js
+++ script.js
@@ -1,27 +1,71 @@
 //RECIBIR DATOS
-
 const url = "https://script.google.com/macros/s/AKfycbwc3WRPOpt0hPDOMawGrk2GhSf5nohK40w0yT80gyjs-I7UqR_ybkzkZTCilf5J6ksodA/exec";
 const recursosPag = fetch(url);
 let recursJSON;
 let datos = document.getElementById("datos");
-let cosa= {};
-function probando2(){
-    return recursosPag
-        .then(resp => resp.json())
-        .then(json => structuredClone(json , cosa) );
+let gasto;
+
+recursosPag
+     .then(resp => resp.json())
+     .then(json => gasto = json );
+     //.then(loadDatos);
+
+
+let gastoPrueba = {
+  mensual:{
+    total:19680.0, 
+    detalle: [[3600.0], [0.0], [0.0], [350.0], [1000.0], [1270.0], [130.0], [3110.0], [0.0], [790.0], [400.0], [1300.0], [2250.0], [3300.0], [0.0], [1200.0], [0.0], [980.0], [0.0], [0.0]]
+  }, 
+  diario:{
+    total:0.0, 
+    detalle:[[0.0]]
+  }
 }
-probando2()
+let cl = console.log;
+const dialogGastos = document.getElementById("dialogGastos");
+const btn_viewGastos = document.getElementById("btn_viewGastos");
+const btnCloseDialog = document.getElementById("btnCloseDialog");
+const dayTotalGasto = document.getElementById("dayTotalGasto");
+const dayDetalleGasto = document.getElementById("dayDetalleGasto");
+const mesTotalGasto = document.getElementById("mesTotalGasto");
+const mesDetallegasto = document.getElementById("mesDetallegasto");
 
-let btnLlamada = document.getElementById("llamada");
-btnLlamada.addEventListener("click" , ()=>console.log(cosa));
-
-//setInterval(()=>console.log(recursJSON),1000)
-
-
-
-function sleep(mili) {
-    return new Promise (respuesta => setTimeout(respuesta , mili));
+btn_viewGastos.addEventListener("click" , ()=> dialogGastos.setAttribute("open",""));
+btnCloseDialog.addEventListener("click" , ()=> dialogGastos.removeAttribute("open"));
+function loadDatos() {
+  dayTotalGasto.textContent = `Gastos en el dia ${gastoPrueba.diario.total}`;
+  dayDetalleGasto.textContent = `Detalles`;
+  mesTotalGasto.textContent = `Gastos en el mes ${gastoPrueba.mensual.total}`;
+  mesDetallegasto.textContent = `Detalles`;
 }
+loadDatos();
+
+
+
+//utilizar datos
+const diasFaltantes = new Date(new Date().getFullYear(), new Date().getMonth() + 1, 0)
+.getDate() - new Date().getDate();
+const reservas = document.getElementById("reservas");
+const daysFaltan = document.getElementById("daysFaltan");
+const recomend = document.getElementById("recomend");
+const dispoDay = document.getElementById("dispoDay");
+const dispoMes = document.getElementById("dispoMes");
+
+
+function usarDatos() {
+  reservas.textContent = `Reservas `
+  daysFaltan.textContent = `Faltan ${diasFaltantes} dias`
+  recomend.textContent = `Recomendado gastar hasta: `
+  dispoDay.textContent = `Disponible total para el dia: `
+  dispoMes.textContent = `Disponible total para el mes: `
+}
+usarDatos();
+
+//let btnLlamada = document.getElementById("llamada");
+//btnLlamada.addEventListener("click" , ()=>console.log(gasto));
+
+
+
 
 
 
