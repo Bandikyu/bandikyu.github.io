ea105fc984a94a07d79659c14cbaaf744ab2163b
Bru <bripoll.18@gmail.com>
2022-04-25 00:28:49 -0300

    cambios 1

--- script.js
+++ script.js
@@ -14,45 +14,95 @@ recursosPag
 let gastoPrueba = {
   mensual:{
     total:19680.0, 
-    detalle: [[3600.0], [0.0], [0.0], [350.0], [1000.0], [1270.0], [130.0], [3110.0], [0.0], [790.0], [400.0], [1300.0], [2250.0], [3300.0], [0.0], [1200.0], [0.0], [980.0], [0.0], [0.0]]
-  }, 
+    detalle:[
+    [3600 ,	'2022-04-01T03:00:00.000Z'],
+    [0 ,	'2022-04-02T03:00:00.000Z'],
+    [0 ,	'2022-04-03T03:00:00.000Z'],
+    [350 ,	'2022-04-04T03:00:00.000Z'],
+    [1000 ,	'2022-04-05T03:00:00.000Z'],
+    [1270 ,	'2022-04-06T03:00:00.000Z'],
+    [130 ,	'2022-04-07T03:00:00.000Z'],
+    [3110 ,	'2022-04-08T03:00:00.000Z'],
+    [0 ,	'2022-04-09T03:00:00.000Z'],
+    [790 ,	'2022-04-10T03:00:00.000Z'],
+    [400 ,	'2022-04-11T03:00:00.000Z'],
+    [1300 ,	'2022-04-12T03:00:00.000Z'],
+    [2250 ,	'2022-04-13T03:00:00.000Z'],
+    [3300 ,	'2022-04-14T03:00:00.000Z'],
+    [0 ,	'2022-04-15T03:00:00.000Z'],
+    [1200 ,	'2022-04-16T03:00:00.000Z'],
+    [0 ,	'2022-04-17T03:00:00.000Z'],
+    [980 ,	'2022-04-18T03:00:00.000Z'],
+    [0 ,	'2022-04-19T03:00:00.000Z'],
+    [0 ,	'2022-04-20T03:00:00.000Z'],
+    ['' ,	'2022-04-21T03:00:00.000Z'],
+    ['' ,	'2022-04-22T03:00:00.000Z'],
+    ['' ,	'2022-04-23T03:00:00.000Z'],
+    ['' ,	'2022-04-24T03:00:00.000Z'],
+    ['' ,	'2022-04-25T03:00:00.000Z'],
+    ['' ,	'2022-04-26T03:00:00.000Z'],
+    ['' ,	'2022-04-27T03:00:00.000Z'],
+    ['' ,	'2022-04-28T03:00:00.000Z'],
+    ['' ,	'2022-04-29T03:00:00.000Z'],
+    ['' ,	'2022-04-30T03:00:00.000Z'],
+    ['' ,	'2022-05-01T03:00:00.000Z'],
+    ]}, 
   diario:{
     total:0.0, 
     detalle:[[0.0]]
   }
 }
+
+//gastos
+function depurando() {
+  gastoPrueba.mensual.detalle.forEach(e=>console.log(`${e[0]} ${e[1].slice(0,10)}`));
+}
+
 let cl = console.log;
 const dialogGastos = document.getElementById("dialogGastos");
 const btn_viewGastos = document.getElementById("btn_viewGastos");
 const btnCloseDialog = document.getElementById("btnCloseDialog");
+const mesTotalGasto = document.getElementById("mesTotalGasto");
+const dayTotalGasto = document.getElementById("dayTotalGasto");
+
+
+const detallesGasto = document.getElementById("detallesGasto");
 const mesDetallegasto = document.getElementById("mesDetallegasto");
 const dayDetalleGasto = document.getElementById("dayDetalleGasto");
-const dayTotalGasto = document.getElementById("dayTotalGasto");
 const btnDetalleDia = document.getElementById("btnDetalleDia");
-const mesTotalGasto = document.getElementById("mesTotalGasto");
 const btnDetalleMes = document.getElementById("btnDetalleMes");
 
 
 
-
-btn_viewGastos.addEventListener('click' , ()=> dialogGastos.setAttribute("open",""));
-btnCloseDialog.addEventListener("click" , ()=> dialogGastos.removeAttribute("open"));
+//total gastos
+btn_viewGastos.addEventListener('click' , ()=> dialogGastos.setAttribute('open',""));
+btnCloseDialog.addEventListener('click' , ()=> dialogGastos.removeAttribute('open'));
 
 function loadDatos() {
   dayTotalGasto.textContent = `Gastos en el dia ${gastoPrueba.diario.total}`;
   mesTotalGasto.textContent = `Gastos en el mes ${gastoPrueba.mensual.total}`;
 
-  //dayDetalleGasto.textContent = `Detalles`;
-  //mesDetallegasto.textContent = `Detalles`;
+  //detalles
+  gastoPrueba.mensual.detalle.forEach(e=>insertNewElement("li",`${e[0]} ${e[1].slice(0,10)}`,mesDetallegasto));
+  gastoPrueba.diario.detalle.forEach(e=>insertNewElement("li",e,dayDetalleGasto));
 
-  //gastoPrueba.mensual.detalle.forEach(e=> mesDetallegasto.appendChild(elementAndContent('p' , e)))
   
 }
-//loadDatos();
+loadDatos();
+
+
+  
+
+/*RESOLVER #Lista Detalles Gasto: 
+tengo que buscar una manera optima de que el efecto de movimiento al usar move() se de
+falla porque se ejecutan al "mismo tiempo" entonces no se muestra el efecto de transicion*/
+btnDetalleDia.addEventListener('click', ()=>{changeDisplay(dayDetalleGasto,"block","none");(move(dayDetalleGasto))});
+
+btnDetalleMes.addEventListener('click', ()=>{changeDisplay(mesDetallegasto  ,"block","none");(move(mesDetallegasto))});
 
 
 
-//utilizar datos
+//info general
 const diasFaltantes = new Date(new Date().getFullYear(), new Date().getMonth() + 1, 0)
 .getDate() - new Date().getDate();
 const reservas = document.getElementById("reservas");
@@ -70,14 +120,6 @@ function usarDatos() {
   dispoMes.textContent = `Disponible total para el mes: `
 }
 usarDatos();
-
-//let btnLlamada = document.getElementById("llamada");
-//btnLlamada.addEventListener("click" , ()=>console.log(gasto));
-
-
-
-
-
 
 
 
@@ -113,9 +155,29 @@ function addGS(x) {
 }
 
 //UTILIDADES
-//crea un elemento HTML y su contenido segun los argumentos
+//crea un elemento HTML y su contenido en un nodo padre segun los argumentos
+function insertNewElement(newElement,textContent,parentNode) {
 
-  let tag = document.createElement("p");
+  let nodeElement = document.createElement(newElement);
+  let nodeText = document.createTextNode(textContent);
+  nodeElement.appendChild(nodeText);
+  parentNode.appendChild(nodeElement);
+
+}
+//selecciona un elemento al que quieras cambiar entre dos display options
+function changeDisplay(element,firstDisplay,secondDisplay) {
+
+  element.style.display == firstDisplay ? element.style.display = secondDisplay : element.style.display = firstDisplay;
+  
+}
+  //la uso para complementar el efecto de 
+  function move (element) {
+    if (element.style.marginLeft == "0px"){
+      element.style.marginLeft = "-50vw";
+    } else element.style.marginLeft = "0";
+  } 
+
+
 
 
 
